5ca904cc3da13c1005232d2e8c0504b439afab4b
Hagerezz <queen.emilly12@gmail.com>
2023-06-05 15:43:08 -0700

    h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - hecks if a singly linked list has a cycle in it
+ * @list: pointer
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	size_t arr_size = 100;
+	int idx = 1, i;
+	listint_t **arr, **arr_cpy;
+
+	arr = malloc(arr_size * sizeof(listint_t*));
+	arr[0] = list;
+	while (list->next != NULL)
+	{
+		if (idx == arr_size)
+		{
+			arr_size += 100;
+			arr_cpy = malloc(arr_size * sizeof(lisint_t*));
+			for (i = 0; i < idx; i++)
+			{
+				arr_cpy[idx] = arr[idx];
+			}
+			free(arr);
+			arr = arr_cpy;
+			arr_cpy = NULL;
+		}
+		for (i = 0; i < idx; i++)
+		{
+			if (arr[i] == list->next)
+			{
+				free(arr);
+				return (1);
+			}
+		}
+		arr[idx++] = list->next;
+		list = list->next;
+	}
+	free(arr);
+	return (0);
+}
