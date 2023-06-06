43cca6309814292c80905cca90e51f14ef9f3772
Hagerezz <queen.emilly12@gmail.com>
2023-06-06 16:59:00 -0700

    hi0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-numbers = number % 10
-if numbers > 5:
+if number > 0:
+    numbers = number % 10
+else:
+    numbers = (-number % 10) * -1
+if abs(numbers) > 5:
     hi = "and is greater than 5"
-elif numbers == 0:
+elif abs(numbers) == 0:
     hi = "and is 0"
 else:
     hi = "and is less than 6 and not 0"
