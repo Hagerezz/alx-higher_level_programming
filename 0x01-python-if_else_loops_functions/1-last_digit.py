3747deab5a05579efaead10b2f3643157b0a3004
Hagerezz <queen.emilly12@gmail.com>
2023-06-06 17:04:15 -0700

    hi0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ if number > 0:
     numbers = number % 10
 else:
     numbers = (-number % 10) * -1
-if abs(numbers) > 5:
+if numbers > 5:
     hi = "and is greater than 5"
-elif abs(numbers) == 0:
+elif numbers == 0:
     hi = "and is 0"
 else:
     hi = "and is less than 6 and not 0"
