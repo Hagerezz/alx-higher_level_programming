568454bd672d5e6cc3fe32387084765f0878ecb9
Hagerezz <queen.emilly12@gmail.com>
2023-06-12 18:50:19 -0700

    hi

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        s = ""
-        for c in row:
-            s += str(c) + " "
-        print("{}".format(s[:-1]))
+        for i in range(len(row) - 1):
+            print("{:d}".format(row[i]), end=" ")
+        print("{:d}".format(row[-1]))
