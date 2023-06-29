30c67f51ae02412324fc4888e7b25aac518e49a0
Hagerezz <queen.emilly12@gmail.com>
2023-06-28 17:47:10 -0700

    hi

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-def tosub(list_n):
-    to_su = 0
-    max_l = max(list_n)
-    for i in list_n:
-        if max_l > i:
-            to_su += i
-    return (max_l - to_su)
+def get_subtraction_value(numbers):
+    subtract_total = 0
+    max_num = max(numbers)
 
-def roman_to_int(roman_string):
-    if not roman_string:
+    subtract_total = sum(filter(lambda n: max_num > n, numbers))
+
+    return (max_num - subtract_total)
+
+
+def roman_to_int(roman_numeral):
+    if not isinstance(roman_numeral, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_key = list(rom_n.key())
-    nu = 0
-    last_ro = 0
-    list_nu = [0]
-    for c in roman_string:
-        for r in list_key:
-            if r == c:
-                if rom_n.get(c) <= last_ro:
-                    nu += to_subtract(list_nu)
-                    list_nu = [rom_n.get(c)]
-                else:
-                    list_nu.append(rom_n.get(c))
-                last_ro = rom_n.get(c)
-    nu += tosub(list_nu)
-    return (nu)
+    roman_numerals_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    last_numeral = 0
+    numerals = [0]
+    for numeral in roman_numeral:
+        if numeral not in roman_numerals_dict:
+            return 0
+        if roman_numerals_dict[numeral] > last_numeral:
+            num += get_subtraction_value(numerals)
+            numerals = [roman_numerals_dict[numeral]]
+        else:
+            numerals.append(roman_numerals_dict[numeral])
+
+        last_numeral = roman_numerals_dict[numeral]
+    num += get_subtraction_value(numerals)
+    return num
