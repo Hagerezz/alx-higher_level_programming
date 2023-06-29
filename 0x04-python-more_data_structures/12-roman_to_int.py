f4a71d7225e7dd05abb1f70497aa7cea8fab2973
Hagerezz <queen.emilly12@gmail.com>
2023-06-28 17:50:43 -0700

    hi

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-def get_subtraction_value(numbers):
-    subtract_total = 0
-    max_num = max(numbers)
+def get_subtraction_value(numbers_list):
+    subtraction_value = 0
+    max_number = max(numbers_list)
+    for number in numbers_list:
+        if max_number > number:
+            subtraction_value += number
+    return max_number - subtraction_value
 
-    subtract_total = sum(filter(lambda n: max_num > n, numbers))
 
-    return (max_num - subtract_total)
-
-
-def roman_to_int(roman_numeral):
-    if not isinstance(roman_numeral, str):
+def roman_to_integer(roman_numeral_string):
+    if not roman_numeral_string:
         return 0
-    roman_numerals_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    last_numeral = 0
-    numerals = [0]
-    for numeral in roman_numeral:
-        if numeral not in roman_numerals_dict:
-            return 0
-        if roman_numerals_dict[numeral] > last_numeral:
-            num += get_subtraction_value(numerals)
-            numerals = [roman_numerals_dict[numeral]]
-        else:
-            numerals.append(roman_numerals_dict[numeral])
-
-        last_numeral = roman_numerals_dict[numeral]
-    num += get_subtraction_value(numerals)
-    return num
+    if not isinstance(roman_numeral_string, str):
+        return 0
+    roman_numeral_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numeral_keys = list(roman_numeral_dict.keys())
+    integer_value = 0
+    last_roman_numeral = 0
+    numbers_list = [0]
+    for numeral in roman_numeral_string:
+        for numeral_key in roman_numeral_keys:
+            if numeral_key == numeral:
+                if roman_numeral_dict.get(numeral) <= last_roman_numeral:
+                    integer_value += get_subtraction_value(numbers_list)
+                    numbers_list = [roman_numeral_dict.get(numeral)]
+                else:
+                    numbers_list.append(roman_numeral_dict.get(numeral))
+                last_roman_numeral = roman_numeral_dict.get(numeral)
+    integer_value += get_subtraction_value(numbers_list)
+    return integer_value
