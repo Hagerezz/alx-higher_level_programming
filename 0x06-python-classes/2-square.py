bd2c33b81d5f5e449bb664cb13c90b47a8f58337
Hagerezz <queen.emilly12@gmail.com>
2023-07-04 00:48:25 +0300

    a

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Represents a square"""
+
+
+class Square:
+    """Represents a square"""
+    pass
+
+    def __init__(self, size=0):
+        """
+        new square
+        Args:
+            size (int) : size of new square
+        """
+        self.__size = size
+        try:
+            size = int(size)
+        except ValueError:
+            raise TypeError("size must be an integer")
+        try:
+            size > 0
+        except ValueError:
+            raise ValueError("size must be >= 0")
