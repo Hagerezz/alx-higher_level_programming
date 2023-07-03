8ecb64b78c237805c2775b4fc8d5b7a09535a961
Hagerezz <queen.emilly12@gmail.com>
2023-07-04 00:35:57 +0300

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Represents a square"""
+
+
+class Square:
+    """Represents a square"""
+    pass
+
+    def __init__(self, size):
+        """
+        new square
+        Args:
+            size (int) : size of new square
+        """
+        self.__size = size
