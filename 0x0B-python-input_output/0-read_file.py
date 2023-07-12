51e272be286c1430a61debec5d7a5bf7e1e34ac6
Hagerezz <queen.emilly12@gmail.com>
2023-07-12 03:40:04 +0300

    hi

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """read"""
 
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
