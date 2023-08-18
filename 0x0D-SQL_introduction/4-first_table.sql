464490c893271118e124f67de0d72b611219abe4
hagerezz <queen.emilly12@gmail.com>
2023-08-18 04:54:11 +0300

    hi

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,4 +2,8 @@
 
 USE mysql;
 
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+);
