49f7660908b3ce215560038ee0fef3067386ec3d
hagerezz <queen.emilly12@gmail.com>
2023-08-23 02:22:13 +0300

    A

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,3 @@
 -- Script to list all the tables of a database
 
-USE mysql;
-
-SET @dbname = '$database_name';
-
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = @dbname;
+SHOW TABLES;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,7 +3,6 @@
 USE mysql;
 
 CREATE TABLE IF NOT EXISTS first_table (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY (id)
+  id INT,
+  name VARCHAR(256),
 );
