fa50b49f7458f3d5d2ff1ecd4446d619f1954afc
hagerezz <queen.emilly12@gmail.com>
2023-09-29 04:34:47 +0300

    hi

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Usage: ./get_response_body.sh <URL>
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" && curl -s "$1"
