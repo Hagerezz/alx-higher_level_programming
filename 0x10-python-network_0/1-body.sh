43ad5002cae484cf4dd0cc2ed00006518450e157
hagerezz <queen.emilly12@gmail.com>
2023-09-29 04:37:27 +0300

    hi

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Usage: ./get_response_body.sh <URL>
-curl -s -o /dev/null -w "%{http_code}" "$1" && curl -s "$1"
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1") [ "$response" -eq 200 ] && curl -s "$1" || echo "Request failed with status code: $response"
